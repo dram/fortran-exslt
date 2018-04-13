ebc36b5ba014d186ff7bd40477135ab8698f0677
Xin Wang <dram.wang@gmail.com>
2018-04-13 20:40:09 +0800

    Add exslt_date_register, exslt_math_register and exslt_str_register

--- sources/exslt.f90
+++ sources/exslt.f90
@@ -4,6 +4,18 @@ module exslt
   interface
      !! exslt
      !! *****
+     subroutine exslt_date_register() &
+          bind(c, name="exsltDateRegister")
+     end subroutine exslt_date_register
+
+     subroutine exslt_math_register() &
+          bind(c, name="exsltMathRegister")
+     end subroutine exslt_math_register
+
+     subroutine exslt_str_register() &
+          bind(c, name="exsltStrRegister")
+     end subroutine exslt_str_register
+
      subroutine exslt_register_all() &
           bind(c, name="exsltRegisterAll")
      end subroutine exslt_register_all
--- tests/main.f90
+++ tests/main.f90
@@ -6,6 +6,8 @@ program main
   block
     use exslt
 
-    call exslt_register_all()
+    call exslt_date_register()
+    call exslt_math_register()
+    call exslt_str_register()
   end block
 end program main
